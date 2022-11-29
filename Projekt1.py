8748368062ee89f1db0ecaf0bf62e07c3c41ee23
Damian Filipow <filipowdam@gmail.com>
2022-11-29 12:06:12 +0100

    Finished initial logic for menu

--- Projekt1.py
+++ Projekt1.py
@@ -7,7 +7,6 @@ class Player():
         self.strength = strength
         self.agility = agility
         self.endurance = endurance
-        self.id = random.randint(1, 100000)
 
 class Opponent():
     def __init__(self, name, category, strength, agility, endurance):
@@ -17,16 +16,10 @@ class Opponent():
         self.agility = agility
         self.endurance = endurance
 
-class SaveFile():
-    def __init__(self, name_of_save, character_id):
-        self.name_of_save = name_of_save
-        self.character_id = character_id
-
 def make_save():
-    save = SaveFile(name_of_player, player1.id)
-    saves[1] = [save.name_of_save, save.character_id]
+    saves[name_of_player] = new_player
 saves = {}
-main_character_id = 0
+current_character = 0
 print("Welcome in Boxer Simulator\nPress 'Enter' to continue.")
 input()
 print(" New Game\n  Load Game\n     Quit")
@@ -34,8 +27,17 @@ command = input("Select an option by typing in: 'New game', 'Load game', 'Quit':
 while command != "Quit":    
     if command == "New game":
         name_of_player = input("What's your name? ")
+        if name_of_player in saves.keys():
+            name_of_player = input("That name is already taken! Choose a different one: ")
         difficulty_lvl = input("Choose difficulty level:\nEasy\nNormal\nHard\n")
-        player1 = Player(name_of_player, difficulty_lvl)
+        new_player = Player(name_of_player, difficulty_lvl)
+        current_character = new_player
+        make_save()
         break
     elif command == ("Load game"):
-        pass
+        for key in saves.keys():
+            print(key)
+        character_to_load = input("Which character do you want to load back to the ring? ")
+        if character_to_load in saves.keys():
+            current_character = saves.get(character_to_load,)
+        break
