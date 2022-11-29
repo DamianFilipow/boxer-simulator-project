392f7d478ebaad69f0c569ecf5e2b9a5175abe9f
Damian Filipow <filipowdam@gmail.com>
2022-11-29 18:16:26 +0100

    No more placeholders name

--- src/prompts/text.py
+++ src/prompts/text.py
@@ -1,73 +1,75 @@
 #introduction
 bobby = "BOBBY"
 trainee = "TRAINEE"
+def get_text(prompt, **kwargs):
+    return prompts[prompt].format(**kwargs)
 
 prompts = dict(
     bobby_text_1 = """
-    {speaker}
+    {bobby}
     You can't, you can't give up that easily!
     Stand up kid!
     """,
     trainee_text_1 = """
-    {speaker}
+    {trainee}
     It's useless! I am good for nothing!
     I am not talented at all! I can't be like you... nor your previous trainee...
     """,
     bobby_text_2 = """
-    {speaker}
+    {bobby}
     My previous trainee? You mean {current_character} right?
     """,
     trainee_text_2 = """
-    {speaker}
+    {trainee}
     I guess...
     """,
 
     bobby_text_3 = """
-    {speaker}
+    {bobby}
     Let me tell you something kid...
     He has been as talentless as you...
     He was good for nothing too!
     """,
 
     trainee_text_3 = """
-    {speaker}
+    {trainee}
     If- if he was like me, how come he became so great?
     I know I can't be like him...
     """,
 
     bobby_text_4 = """
-    {speaker}
+    {bobby}
     Tell me the numbers!
     """,
 
     trainee_text_4 = """
-    {speaker}
+    {trainee}
     Wh-at what numbers?
     What are you talking about?
     """,
 
     bobby_text_5 = """
-    {speaker}
+    {bobby}
     Lottery numbers, you know them, don't you?
     """,
 
     trainee_text_5 = """
-    {speaker}
+    {trainee}
     Are you mad?!
     Why do you think I know them?
     """,
     bobby_text_6 = """
-    {speaker}
+    {bobby}
     You cleary know the future, otherwise you couldn't tell me what you can't become!
     """,
 
     trainee_text_6 = """
-    {speaker}
+    {trainee}
     ...
     """,
 
     bobby_text_7 = """
-    {speaker}
+    {bobby}
     I believe at you kid!
     I know you can do it too!
     You just need to believe at that too!
@@ -75,11 +77,8 @@ prompts = dict(
     """,
 
     bobby_text_8 = """
-    {speaker}
+    {bobby}
     Let me tell you a story, a story how I met {current_character}.
     Haha... I remember that day as it was yesterday.
     """
 )
-
-def penis(prompt, **kwargs):
-    return prompts[prompt].format(**kwargs)
