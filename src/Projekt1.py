898646b3d876b8c9f9a8421ad2afc756295f1e7a
Damian Filipow <filipowdam@gmail.com>
2022-11-30 20:27:13 +0100

    Added one more class called 'Place' and bit of game initial logic in while loop

--- src/Projekt1.py
+++ src/Projekt1.py
@@ -1,13 +1,19 @@
 import prompts.text as txt
 
 class Player():
-    def __init__(self, name, difficulty, category = "Newbie", strength = 10, agility = 10, endurance = 10):
+    def __init__(self, name, difficulty, location = "Home", category = "Newbie", strength = 10, agility = 10, endurance = 10):
         self.name = name
         self.difficulty = difficulty
         self.category = category
         self.strength = strength
         self.agility = agility
         self.endurance = endurance
+        self.energy = 100
+        self.money_amount = 50
+        self.location = "Home"
+        self.inventory = {}
+        self.status = {"Name:": name, "Category:": category, "Strength:": strength, "Agility:": agility, "Endurance:": endurance, "Energy:": self.energy, "Money:": self.money_amount}
+        self.location = location
 
 class Opponent():
     def __init__(self, name, category, strength, agility, endurance):
@@ -15,15 +21,43 @@ class Opponent():
         self.category = category
         self.strength = strength
         self.agility = agility
-        self.endurance = endurance
+        self.endurance = endurance   
+
+class Place():
+    def __init__(self, name, is_shop, is_gym, is_home):
+        self.name = name
+        self.is_shop = is_shop
+        self.is_gym = is_gym
+        self.is_home = is_home
 
 def make_save():
     saves[name_of_player] = new_player
 
+def call_command(command):
+    
+    if command == "s":
+        for key, value in current_character.status.items():
+            print(key, value)
+       
+    elif command == "m":
+        print(current_character.location)
+        
+    elif command == "i":
+        for key, value in current_character.inventory.items():
+            print(key, value)
+    elif command == "q":
+        return command
 
 def print_formated_text(s,**kwargs):
     print(txt.get_text(s,**kwargs))
     input()
+
+locations = dict(
+    home = Place("Home", False, False, True),
+    gym = Place("Gym", False, True, False),
+    shop = Place("Shop", True, False, False) 
+)
+
 saves = {}
 current_character = 0
 
@@ -31,6 +65,7 @@ print("Welcome in Boxer Simulator\nPress 'Enter' to continue.")
 input()
 print(" New Game\n  Load Game\n     Quit")
 command = input("Select an option by typing in: 'New game', 'Load game', 'Quit': ")
+
 while command != "Quit":
     if command == "New game":
         name_of_player = input("What's your name? ")
@@ -52,3 +87,7 @@ while command != "Quit":
 if command == "New game":
     for text in txt.prompts.keys():
         print_formated_text(text, bobby=txt.bobby, trainee=txt.trainee, current_character=current_character.name)
+
+print(txt.commands)
+while call_command(input()) != "q":
+    pass
